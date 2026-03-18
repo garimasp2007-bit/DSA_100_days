5eeac44cbdecb5e5dac0590ff4fb7f1de7df53dc
garimasp2007-bit <garima.sp2007@gmail.com>
2026-03-18 11:40:33 +0530

    commit

---
+++ 3.c
@@ -0,0 +1,58 @@
+/*roblem: Implement linear search to find key k in an array. Count and display the number of comparisons performed.
+
+Input:
+- First line: integer n (array size)
+- Second line: n space-separated integers
+- Third line: integer k (key to search)
+
+Output:
+- Line 1: "Found at index i" OR "Not Found"
+Line 2: "Comparisons = c"
+
+Example:
+Input:
+5
+10 20 30 40 50
+30
+
+Output:
+Found at index 2
+Comparisons = 3
+
+Explanation: Compared with 10, 20, 30 (found at index 2 with 3 comparisons)*/
+#include <stdio.h>
+
+int main() {
+    int n, k, found = 0, comparisons = 0;
+
+    scanf("%d", &n);
+
+    int arr[100];
+
+    // Input array
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Input key
+    scanf("%d", &k);
+
+    // Linear Search
+    for(int i = 0; i < n; i++) {
+        comparisons++;   // count each comparison
+
+        if(arr[i] == k) {
+            printf("Found at index %d\n", i);
+            found = 1;
+            break;
+        }
+    }
+
+    if(found == 0) {
+        printf("Not Found\n");
+    }
+
+    printf("Comparisons = %d", comparisons);
+
+    return 0;
+}
